2a6b192c4c84a9b74a0ee5b6d4e684f1a62d44f3
Asshen <Asshen4522@yandex.ru>
2023-01-24 22:56:11 +0300

    личный кабинет, начало

--- resources/js/app.vue
+++ resources/js/app.vue
@@ -1,12 +1,17 @@
 <script setup>
 import Authorization from "./pages/Authorization.vue";
 import CreateOrder from "./pages/CreateOrder.vue";
+import ManagerCabinet from "./pages/ManagerCabinet.vue";
 
 import { reactive, ref } from "vue";
 
 const local_data = reactive({
-    currentPage: 2,
+    currentPage: 3,
 });
+
+function changePage(index) {
+    local_data.currentPage = index;
+}
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 </script>
@@ -18,6 +23,9 @@ const local_data = reactive({
         </div>
         <div v-if="local_data.currentPage === 2">
             <CreateOrder />
+        </div>
+        <div v-if="local_data.currentPage === 3">
+            <ManagerCabinet @openPage="changePage" />
         </div>
     </div>
 </template>
---
+++ resources/js/pages/ManagerCabinet.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { reactive } from "vue";
+
+const emit = defineEmits(["openPage"]);
+
+function nextPage(index) {
+    emit("openPage", index);
+}
+</script>
+<template>
+    <div>
+        <button @click="nextPage(2)">Добавить заказ</button>
+        <button>Добавить пользователя</button>
+    </div>
+    <div></div>
+</template>
+<style scoped></style>
